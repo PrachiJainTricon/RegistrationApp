aad3b6926db99d5a87b6721e6e68ec5abe9de89e
PrachiJainTricon <Prachi.Jain@Triconinfotech.com>
2023-06-21 13:14:24 +0530

    xml data in table

--- src/app/_service/auth.service.ts
+++ src/app/_service/auth.service.ts
@@ -2,15 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private xmlDataUrl = 'assets/User.xml';
 
-  constructor(private router: Router,
-    private http: HttpClient) {
-
-  }
+  constructor(private router: Router, private http: HttpClient) {}
 
   isAunthenticated(): Boolean {
     if (sessionStorage.getItem('token') !== null) {
@@ -20,39 +21,40 @@ export class AuthService {
   }
 
   canAcess() {
-    if (!(this.isAunthenticated())) {
-      this.router.navigate(['/login'])
+    if (!this.isAunthenticated()) {
+      this.router.navigate(['/login']);
     }
   }
 
-  canAuthenticate(process: string, response?: string,) {
+  canAuthenticate(process: string, response?: string) {
     if (process === 'register') {
-      if ((this.isAunthenticated())) {
-        this.router.navigate(['/dashboard'])
+      if (this.isAunthenticated()) {
+        this.router.navigate(['/dashboard']);
       }
-    }
-    else if (process === 'login') {
+    } else if (process === 'login') {
       if (response === 'authenticated') {
-        if ((this.isAunthenticated())) {
-          this.router.navigate(['/dashboard'])
+        if (this.isAunthenticated()) {
+          this.router.navigate(['/dashboard']);
         }
       }
     }
   }
 
   register(name: string, email: string, password: string) {
-    return this.http
-      .post<{ email: string }>("http://localhost:8085/api/user/save",
-        { displayName: name, email: email, password: password }
-      )
+    return this.http.post<{ email: string }>(
+      'http://localhost:8085/api/user/save',
+      { displayName: name, email: email, password: password }
+    );
   }
 
   login(email: string, password: string) {
     return this.http.post<{ authenticate: string }>(
-      "http://localhost:8085/api/user/login", {
-      email, password
-    }
-    )
+      'http://localhost:8085/api/user/login',
+      {
+        email,
+        password,
+      }
+    );
   }
 
   storeResponse(data: string) {
@@ -62,5 +64,21 @@ export class AuthService {
     sessionStorage.removeItem('token');
   }
 
-
+  getXmlData(): Observable<any[]> {
+    return this.http.get(this.xmlDataUrl, { responseType: 'text' }).pipe(
+      map((xmlString) => {
+        const parser = new DOMParser();
+        const xml = parser.parseFromString(xmlString, 'text/xml');
+        const users = Array.from(xml.getElementsByTagName('user'));
+        return users.map((user) => {
+          const id = user.getElementsByTagName('id')[0].textContent;
+          const name = user.getElementsByTagName('name')[0].textContent;
+          const city = user.getElementsByTagName('city')[0].textContent;
+          const mobileNumber =
+            user.getElementsByTagName('mobileNumber')[0].textContent;
+          return { id, name, city, mobileNumber };
+        });
+      })
+    );
+  }
 }
